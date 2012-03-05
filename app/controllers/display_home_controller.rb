2e13a82c88c67a271a61271b480b727a3666197b
Ardo K <ardo1201@yahoo.com>
2012-03-04 22:27:36 -0800

    change the products view per page from 15 to 16

--- app/controllers/display_home_controller.rb
+++ app/controllers/display_home_controller.rb
@@ -12,17 +12,17 @@ class DisplayHomeController < ApplicationController
   end
 
   ##
-  # @param	items_per_page	Optional. Default 15. Number of items to grab
+  # @param	items_per_page	Optional. Default 16. Number of items to grab
   #	@param	page_num	Optional. Default 1 (1-based). The page number
   ##
   def products
-  	items_per_page = params[:items_per_page] || 15
+  	items_per_page = params[:items_per_page] || 16
   	page_num = params[:page_num] || 1
   
     @products = Product.pull(page_num,items_per_page).collect{|p| p.as_json}
     @items_per_page = items_per_page
     @page_num = page_num.to_i
-    @total_items = Product.count    
+    @total_items = Product.count   
      
   end 
 
